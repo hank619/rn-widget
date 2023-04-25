cf68d04f5d55061a46c85fdb60574868c6a68be1
zhanghong <hong.zhang@axinan.com>
2023-04-25 15:44:12 +0800

    FIX: add nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: Hong.Zhang
+ * @Date: 2022-06-13 11:22:08
+ * @Description: 
+ */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: ["nativewind/babel"]
 };
